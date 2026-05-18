afc4c7f6bdb02891627b0f48134390e1a2cba763
kidddjh <23454670+kidddjh@users.noreply.github.com>
2026-05-18 10:06:57 +0800

    Update imgbox_rewrite.js

--- imgbox_rewrite.js
+++ imgbox_rewrite.js
@@ -1,17 +1,7 @@
-/*
- * Quantumult X Script: ImgBox Image Host Rewrite
- * Removes _th from URL to get direct link
- *
- * Usage:
- * 1. Add quantumultx_imagehost_rewrite.conf to Rewrite
- * 2. Save this file to: iCloud/Quantumult X/Scripts/imgbox_rewrite.js
- * 3. Enable in Quantumult X → Scripts
- */
+// Quantumult X Script: ImgBox Image Host Rewrite
+// imgbox.com _th. → 去掉 _th 获取直链
 
 const url = $request.url;
-
-// ImgBox thumbnail:  https://X.images.imgbox.com/XX/XX/XXXX_th.jpg
-// Direct link:       https://X.images.imgbox.com/XX/XX/XXXX.jpg
 
 if (/imgbox\.com/.test(url) && /_th\.\w+$/.test(url)) {
     const newUrl = url.replace(/_th(\.\w+)$/, '$1');
